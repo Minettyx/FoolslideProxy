83ec6dadd07d390e576fd844d14cc8018f5bb741
Minettyx <30463302+Minettyx@users.noreply.github.com>
2024-07-27 17:20:16 +0200

    readded Dockerfile + changed flake

--- flake.nix
+++ flake.nix
@@ -1,50 +1,42 @@
 {
-  description = "FoolslideProxy";
+  description = "Proxy that converts manga reading websites to foolslide (for Tachiyomi) ";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-
-        nativeBuildInputs = with pkgs; [
+  outputs = {nixpkgs, ...}: let
+    lib = nixpkgs.lib;
+    systems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      "aarch64-linux"
+    ];
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs systems (system:
+      import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      });
+  in {
+    devShells = forEachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
           go
           gopls
+          templ
         ];
-        buildInputs = with pkgs; [];
+      };
+    });
 
-        goModule = pkgs.buildGoModule rec {
-          name = "foolslideproxy";
-          src = ./.;
-          inherit buildInputs;
-          vendorHash = "sha256-hEyV9aSe5jypKvZMt1doejQd8ARAL1hZEl0LTmQUPlo=";
-        };
-
-        dockerImage = pkgs.dockerTools.buildLayeredImage {
-          name = "foolslideproxy";
-          contents = with pkgs; [
-            busybox
-            dockerTools.binSh
-          ];
-          config = {
-            Cmd = ["${goModule}/bin/foolslideproxy"];
-          };
-        };
-      in {
-        devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
-
-        packages = {
-          default = goModule;
-          docker = dockerImage;
-        };
-      }
-    );
+    packages = forEachSystem (pkgs: {
+      default = pkgs.buildGoModule {
+        name = "foolslideproxy";
+        src = ./.;
+        vendorHash = "sha256-hEyV9aSe5jypKvZMt1doejQd8ARAL1hZEl0LTmQUPlo=";
+        nativeBuildInputs = [];
+      };
+    });
+  };
 }
